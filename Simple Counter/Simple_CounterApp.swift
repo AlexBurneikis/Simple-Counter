e4ca65b9f8cf16ed4af44c8645d561fcb3b01318
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-06-06 09:27:36 +1000

    Initial Commit

---
+++ Simple Counter/Simple_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_CounterApp.swift
+//  Simple Counter
+//
+//  Created by Alexander Burneikis on 6/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct Simple_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
